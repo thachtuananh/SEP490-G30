8a6c88fba1f33b4f6df1b9a2a9407b5e12e8d1ed
Anh Thach <anhtthe161427@fpt.edu.vn>
2025-01-23 21:38:22 +0700

    init

---
+++ HomeCleanAPI/src/main/java/com/example/homecleanapi/HomeCleanApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.homecleanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeCleanApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeCleanApiApplication.class, args);
+    }
+
+}
---
+++ HomeCleanAPI/src/test/java/com/example/homecleanapi/HomeCleanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homecleanapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeCleanApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
