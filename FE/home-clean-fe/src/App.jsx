1def5997518b0cb7ef38d8d21985d4b16c483949
NgynSon <sonnxhe163813@fpt.edu.vn>
2025-02-06 20:52:51 +0700

    init FE

---
+++ FE/home-clean-fe/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to React</h1>
+        <p>Edit <code>src/App.jsx</code> and save to reload.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
