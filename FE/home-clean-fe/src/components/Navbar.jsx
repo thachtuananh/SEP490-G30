6f534f9ba4e25918148d1c541581fee4002ab573
NgynSon <sonnxhe163813@fpt.edu.vn>
2025-02-13 19:42:16 +0700

    Update navbar

--- FE/home-clean-fe/src/components/Navbar.jsx
+++ FE/home-clean-fe/src/components/Navbar.jsx
@@ -2,47 +2,51 @@ import { Link } from "react-router-dom"
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="logo">
-        <Link to="/">
-          <img
-            src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/HouseClean_logo-hrG7QBW49ADjlEI47AgaKbmxcfpQcv.png"
-            alt="House Clean Logo"
-            className="logo-img"
-          />
+    <div className="Container">
+      <nav className="navbar">
+        <div className="logo">
+          <Link to="/">
+            <img
+              src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/HouseClean_logo-hrG7QBW49ADjlEI47AgaKbmxcfpQcv.png"
+              alt="House Clean Logo"
+              className="logo-img"
+            />
+          </Link>
+        </div>
+
+        <ul className="menu">
+          <li>
+            <Link to="/" className="nav-link">
+              Giới thiệu
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="nav-link">
+              Tất cả dịch vụ
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="nav-link">
+              Theo dõi dịch vụ
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="nav-link">
+              Tin tức
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="nav-link">
+              Liên hệ
+            </Link>
+          </li>
+        </ul>
+
+        <Link to="/login" className="login-btn">
+          Đăng nhập
         </Link>
-      </div>
-      <ul className="menu">
-        <li>
-          <Link to="/" className="nav-link">
-            Giới thiệu
-          </Link>
-        </li>
-        <li>
-          <Link to="/" className="nav-link">
-            Tất cả dịch vụ
-          </Link>
-        </li>
-        <li>
-          <Link to="/" className="nav-link">
-            Theo dõi dịch vụ
-          </Link>
-        </li>
-        <li>
-          <Link to="/" className="nav-link">
-            Tin tức
-          </Link>
-        </li>
-        <li>
-          <Link to="/" className="nav-link">
-            Liên hệ
-          </Link>
-        </li>
-      </ul>
-      <Link to="/login" className="login-btn">
-        Đăng nhập
-      </Link>
-    </nav>
+      </nav>
+    </div>
   )
 }
 
