c5ae5711998b90606666adc1a9b892c0f0d5fe92
Anh Thach <anhtthe161427@fpt.edu.vn>
2025-02-24 22:37:47 +0700

    Fix cors and update API cleaner

--- HomeCleanAPI/src/main/java/com/example/homecleanapi/controllers/CustomerController.java
+++ HomeCleanAPI/src/main/java/com/example/homecleanapi/controllers/CustomerController.java
@@ -1,13 +1,12 @@
 package com.example.homecleanapi.controllers;
 
-import com.example.homecleanapi.dtos.CustomerProfileRequest;
-import com.example.homecleanapi.dtos.ForgotPasswordRequest;
-import com.example.homecleanapi.dtos.LoginRequest;
-import com.example.homecleanapi.dtos.CustomerRegisterRequest;
+import com.example.homecleanapi.dtos.*;
+import com.example.homecleanapi.models.CustomerAddresses;
 import com.example.homecleanapi.services.CustomerAuthService;
-import com.example.homecleanapi.services.CustomerProfileService;
+import com.example.homecleanapi.services.CustomerService;
 import com.example.homecleanapi.utils.JwtUtils;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,40 +19,62 @@ import java.util.Map;
 public class CustomerController {
 
     private JwtUtils jwtUtils;
-
     private final CustomerAuthService customerAuthService;
+    private CustomerService customerService;
 
-    private CustomerProfileService customerProfileService;
 
-    public CustomerController(CustomerAuthService customerAuthService, JwtUtils jwtUtils, CustomerProfileService customerProfileService) {
+    public CustomerController(CustomerAuthService customerAuthService, JwtUtils jwtUtils, CustomerService customerService) {
         this.customerAuthService = customerAuthService;
         this.jwtUtils = jwtUtils;
-        this.customerProfileService = customerProfileService;
+        this.customerService = customerService;
     }
 
-    @PostMapping("/register")
+    @PostMapping(value = "/register", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Map<String, Object>> register(@RequestBody CustomerRegisterRequest request) {
         return customerAuthService.customerRegister(request);
     }
 
-    @PostMapping("/login")
+    @PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Map<String, Object>> login(@RequestBody LoginRequest request) {
         return customerAuthService.customerLogin(request);
     }
 
-    @PostMapping("/forgot-password")
+    @PostMapping(value = "/forgot-password",  produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Map<String, Object>> forgotPassword(@RequestBody ForgotPasswordRequest request) {
         return customerAuthService.customerForgotPassword(request);
     }
 
-    @PutMapping("/{customer_id}/profile")
+    @PutMapping(value = "/{customer_id}/profile",  produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Map<String, Object>> updateProfile(@RequestBody CustomerProfileRequest request, @RequestParam int customer_id) {
 
-        return customerProfileService.updateProfile(customer_id, request);
+        return customerService.updateProfile(customer_id, request);
     }
 
-    @GetMapping("/{customer_id}/profile")
+    @GetMapping(value = "/{customer_id}/profile", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Map<String, Object>> getProfile(@RequestParam int customer_id) {
-        return customerProfileService.getProfile(customer_id);
+        return customerService.getProfile(customer_id);
+    }
+
+    @PostMapping(value = "/{customer_id}/create-address",  produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Object>> createAddress(@RequestBody CustomerAddressesDTO request, @RequestParam int customer_id) {
+        return customerService.addAddress(request, customer_id);
+    }
+
+    @PutMapping(value = "/{customer_id}/update-address", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Object>> updateEmployeeAddress(@RequestBody CustomerAddressesDTO request, @RequestParam int employeeId) {
+        // Gọi service để xử lý update địa chỉ
+        return customerService.updateCustomerAddress(request, employeeId);
+    }
+
+    // API xóa địa chỉ theo locationId
+    @DeleteMapping(value = "/{locationId}/delete_address", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Object>> deleteEmployeeAddress(@PathVariable int locationId) {
+        return customerService.deleteCustomerAddress(locationId);
+    }
+
+    // API lấy danh sách địa chỉ của employee theo employeeId
+    @GetMapping(value = "/{customer_id}/all-addresses",  produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Object>> getAllEmployeeAddresses(@PathVariable int employeeId) {
+        return customerService.getAllCusomterAddresses(employeeId);
     }
 }
