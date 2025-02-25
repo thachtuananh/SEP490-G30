c3fa9552c7457eda2ba38ea05e38e46952d17409
Anh Thach <anhtthe161427@fpt.edu.vn>
2025-02-25 21:37:25 +0700

    Hot fix

--- HomeCleanAPI/src/main/java/com/example/homecleanapi/security/SecurityConfig.java
+++ HomeCleanAPI/src/main/java/com/example/homecleanapi/security/SecurityConfig.java
@@ -59,6 +59,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/employee/login").permitAll()
                         .requestMatchers("/api/employee/register").permitAll()
                         .requestMatchers("/api/employee/forgot-password").permitAll()
+                        .requestMatchers("/api/services/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                         .requestMatchers("/api-docs").permitAll()
                         .anyRequest().authenticated()
