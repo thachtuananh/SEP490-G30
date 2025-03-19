f43560a9a8370de446e5c86a606012bd8a77fb96
Anh Thach <ohohoh20052002@gmail.com>
2025-03-19 14:39:47 +0700

    Update script

--- HomeCleanAPI/src/main/java/com/example/homecleanapi/controllers/ServiceController.java
+++ HomeCleanAPI/src/main/java/com/example/homecleanapi/controllers/ServiceController.java
@@ -13,7 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "Customer API")
+@Tag(name = "Customer Service API")
 @RestController
 @RequestMapping("/api/services")
 public class ServiceController {
