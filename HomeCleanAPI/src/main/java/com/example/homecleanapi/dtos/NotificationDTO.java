82c4da50727c67760ebfc148952ec6e04b9b0c7f
manhtthe <manhtthe161599@fpt.edu.vn>
2025-03-25 14:05:54 +0700

    for son

--- HomeCleanAPI/src/main/java/com/example/homecleanapi/dtos/NotificationDTO.java
+++ HomeCleanAPI/src/main/java/com/example/homecleanapi/dtos/NotificationDTO.java
@@ -1,15 +1,14 @@
 package com.example.homecleanapi.dtos;
 
-import lombok.AllArgsConstructor;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.List;
+
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class NotificationDTO implements Serializable {
     private Integer userId;
