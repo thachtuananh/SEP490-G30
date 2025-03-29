99e8107f2b701f5e590e475afde6e9a3b62ce0ab
Anh Thach <anhtthe161427@fpt.edu.vn>
2025-03-29 21:10:23 +0700

    Update configs

--- HomeCleanAPI/src/main/java/com/example/homecleanapi/security/SecurityConfig.java
+++ HomeCleanAPI/src/main/java/com/example/homecleanapi/security/SecurityConfig.java
@@ -97,12 +97,12 @@ public class SecurityConfig {
                 "http://34.136.232.226:8080",
                 "https://house-clean-platform.web.app",
                 "https://house-clean-platform.firebaseapp.com",
-                "https://costume-lithuania-parameter-bathrooms.trycloudflare.com")); // 🔥 Thêm domain frontend của bạn
+                "https://costume-lithuania-parameter-bathrooms.trycloudflare.com",
                 "http://34.136.232.226:8080",
                 "https://house-clean-platform.web.app",
                 "https://house-clean-platform.firebaseapp.com",
                 "https://costume-lithuania-parameter-bathrooms.trycloudflare.com",
-                "https://pike-armor-ms-hampton.trycloudflare.com");
+                "https://pike-armor-ms-hampton.trycloudflare.com"));
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:3000", "http://34.121.192.129:8080", "https://house-clean-platform.web.app/", "https://house-clean-platform.firebaseapp.com/")); // 🔥 Thêm domain frontend của bạn
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
