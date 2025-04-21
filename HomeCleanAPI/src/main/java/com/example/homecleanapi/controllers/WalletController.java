b7f04f76356909c7f6c2c61d1db12925c0a26eaa
Anh Thach <anhtthe161427@fpt.edu.vn>
2025-04-21 12:26:24 +0700

    Hot fix wallet

--- HomeCleanAPI/src/main/java/com/example/homecleanapi/controllers/WalletController.java
+++ HomeCleanAPI/src/main/java/com/example/homecleanapi/controllers/WalletController.java
@@ -227,8 +227,11 @@ public class WalletController {
 
             if ("vnpay".equalsIgnoreCase(paymentMethod)) {
                 // Xử lý thanh toán qua VNPay
-                Map<String, Object> response = walletService.depositMoney(customerId, amount, request);
-                return ResponseEntity.ok(response);
+//                Map<String, Object> response =
+                walletService.depositMoney(customerId, amount, request);
+                String redirectUrl = "https://house-clean-platform.web.app/DepositOwner?status=fail";
+                return ResponseEntity.status(HttpStatus.OK).header(HttpHeaders.LOCATION, redirectUrl).build();
+//                return ResponseEntity.ok(response);
             }else {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                         .body(Map.of("message", "Invalid payment method"));
