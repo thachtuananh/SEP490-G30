4d40d86c71fb76320dfe2a94c4f9d5d34ea16bae
SonNX163813 <108038889+SonNX163813@users.noreply.github.com>
2025-05-06 15:32:42 +0700

    Fix UI

--- FE/home-clean-fe/src/components/Admin/cleaner/JobHistoryTable.jsx
+++ FE/home-clean-fe/src/components/Admin/cleaner/JobHistoryTable.jsx
@@ -178,7 +178,7 @@ const JobHistoryTable = ({
           </Col>
           <Col xs={24} sm={8}>
             <Statistic
-              title="Tổng lợi nhuận"
+              title="Tổng doanh thu"
               value={jobData
                 .filter((job) => job.jobStatus === "DONE")
                 .reduce((sum, job) => sum + job.totalPrice, 0)
