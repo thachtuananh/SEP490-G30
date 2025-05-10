696646a0148490f326497d0ae0cbaf9a3505ee48
Ametist <anhthach291@gmail.com>
2025-05-10 13:54:23 +0700

    Update config

--- HomeCleanAPI/src/main/java/com/example/homecleanapi/utils/OpenAPIConfig.java
+++ HomeCleanAPI/src/main/java/com/example/homecleanapi/utils/OpenAPIConfig.java
@@ -20,7 +20,7 @@ public class OpenAPIConfig {
                         .description("API docs with JWT authentication"))
                 .servers(Collections.singletonList( // Đặt URL API mặc định là HTTPS
                         new io.swagger.v3.oas.models.servers.Server()
-                                .url("https://detection-paintings-mistake-makers.trycloudflare.com")
+                                .url("https://grass-finding-presentation-competitions.trycloudflare.com")
                                 .description("Cloudflare Tunnel")))
                 .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
                 .components(new io.swagger.v3.oas.models.Components()
