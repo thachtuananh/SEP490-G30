8203eab85c2fcb2bef3e5bfe613fc9f4aa0ca755
manhtthe <manhtthe161599@fpt.edu.vn>
2025-05-25 21:35:05 +0700

    add pay back via wallet váºer2

--- HomeCleanAPI/src/main/java/com/example/homecleanapi/services/JobService.java
+++ HomeCleanAPI/src/main/java/com/example/homecleanapi/services/JobService.java
@@ -283,13 +283,13 @@ public class JobService {
             }
         }
 
-//        NotificationDTO customerNotification = new NotificationDTO();
-//        customerNotification.setUserId(job.getCustomer().getId());
-//        customerNotification.setMessage("Công việc đã được tạo thành công");
-//        customerNotification.setType("AUTO_MESSAGE");
-//        customerNotification.setTimestamp(LocalDate.now());
-//        customerNotification.setRead(false);
-//        notificationService.processNotification(customerNotification, "CUSTOMER", Math.toIntExact(customerId));
+        NotificationDTO customerNotification = new NotificationDTO();
+        customerNotification.setUserId(job.getCustomer().getId());
+        customerNotification.setMessage("Công việc đã được tạo thành công");
+        customerNotification.setType("AUTO_MESSAGE");
+        customerNotification.setTimestamp(LocalDate.now());
+        customerNotification.setRead(false);
+        notificationService.processNotification(customerNotification, "CUSTOMER", Math.toIntExact(customerId));
 
         response.put("message", "Đặt lịch thành công");
         response.put("jobId", job.getId());
