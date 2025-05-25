258172d5ac1b20d3e5ea3634f54a70622d4f78cc
Ametist <anhthach291@gmail.com>
2025-05-25 13:11:58 +0700

    Hot fix amount 1

--- HomeCleanAPI/src/main/java/com/example/homecleanapi/controllers/WalletController.java
+++ HomeCleanAPI/src/main/java/com/example/homecleanapi/controllers/WalletController.java
@@ -250,8 +250,10 @@ public class WalletController {
 
             if ("vnpay".equalsIgnoreCase(paymentMethod)) {
                 // Xử lý thanh toán qua VNPay
+                DecimalFormat formatter = new DecimalFormat("#,###.##");
+                String formattedAmount = formatter.format(amount);
                 Map<String, Object> response = walletService.depositMoney(customerId, amount, request);
-                String message = "Bạn đã nạp: " +  amount + " VND vào ví thành công";
+                String message = "Bạn đã nạp: " + formattedAmount  + " VND vào ví thành công";
                 NotificationDTO customerNotification = new NotificationDTO();
                 customerNotification.setUserId(Math.toIntExact(customerId));
                 customerNotification.setMessage(message);
