ce52b69d36ed3198e60c00fe8f0dbd8def715e66
SonNX163813 <108038889+SonNX163813@users.noreply.github.com>
2025-05-25 20:23:46 +0700

    Hot fix notification

--- FE/home-clean-fe/src/components/Notification/Notification.jsx
+++ FE/home-clean-fe/src/components/Notification/Notification.jsx
@@ -145,7 +145,7 @@ const Notification = ({
             Thông báo
           </Title>
           <div style={{ display: "flex", gap: "8px" }}>
-            {hasUnreadNotifications && (
+            {/* {hasUnreadNotifications && (
               <CheckOutlined
                 onClick={handleMarkAllAsRead}
                 style={{
@@ -155,7 +155,7 @@ const Notification = ({
                 }}
                 title="Đánh dấu tất cả đã đọc"
               />
-            )}
+            )} */}
             {notifications.length > 0 && (
               <DeleteOutlined
                 onClick={handleClearAllNotifications}
