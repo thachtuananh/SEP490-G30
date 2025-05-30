c403dd95e6ffb1ecc6b4f9a83184e64ed455dc3e
manhtthe <manhtthe161599@fpt.edu.vn>
2025-05-30 13:28:58 +0700

    new

--- HomeCleanAPI/src/main/java/com/example/homecleanapi/services/CleanerJobService.java
+++ HomeCleanAPI/src/main/java/com/example/homecleanapi/services/CleanerJobService.java
@@ -2019,6 +2019,7 @@ public class CleanerJobService {
 			jobInfo.put("status", job.getStatus()); // in status thực tế của job
 			jobInfo.put("scheduledTime", job.getScheduledTime());
 			jobInfo.put("totalPrice", job.getTotalPrice());
+			jobInfo.put("orderCode", job.getOrderCode());
 
 			Customers customer = job.getCustomer();
 			if (customer != null) {
