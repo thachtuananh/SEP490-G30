5f2aaa48bc447aaecff262c76a0557bd9881e93a
Ametist <anhthach291@gmail.com>
2025-06-01 01:36:29 +0700

    Hot fix concurrent job 10

--- HomeCleanAPI/src/main/java/com/example/homecleanapi/services/CleanerJobService.java
+++ HomeCleanAPI/src/main/java/com/example/homecleanapi/services/CleanerJobService.java
@@ -904,13 +904,13 @@ public class CleanerJobService {
 		Optional<JobApplication> jobApplicationOpt = jobApplicationRepository.findByJobAndCleaner(job, cleaner);
 		if (!jobApplicationOpt.isPresent() || !jobApplicationOpt.get().getStatus().equals("Accepted")) {
 			response.put("message", "Công việc đã bị huỷ");
-			return ResponseEntity.badRequest().body(response);
+			return ResponseEntity.notFound().build();
 		}
 
 		// Kiểm tra trạng thái của công việc
 		if (!job.getStatus().equals(JobStatus.ARRIVED)) {
 			response.put("message", "Job is not in 'IN_PROGRESS' state");
-			return ResponseEntity.badRequest().body(response);
+			return ResponseEntity.notFound().build();
 		}
 
 //		List<JobServiceDetail> jobServiceDetails = jobDetailsRepository.findByJob_id(jobId);
