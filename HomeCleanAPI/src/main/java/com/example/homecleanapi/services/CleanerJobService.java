b77ba437b1bb7dc2dbae699273ee5b2530f23227
Ametist <anhthach291@gmail.com>
2025-06-01 03:39:28 +0700

    Hot fix setJobArrived

--- HomeCleanAPI/src/main/java/com/example/homecleanapi/services/CleanerJobService.java
+++ HomeCleanAPI/src/main/java/com/example/homecleanapi/services/CleanerJobService.java
@@ -840,7 +840,7 @@ public class CleanerJobService {
 		customerNotification.setTimestamp(LocalDateTime.now());
 		customerNotification.setRead(false); // ✅ set read = false
 		notificationService.processNotification(customerNotification, "CUSTOMER", Math.toIntExact(job.getCustomer().getId()));
-		// smsService.sendSms(job.getCustomer().getPhone(), message);
+		smsService.sendSms(job.getCustomer().getPhone(), message);
 
 		String message_cleaner = "[Mã công việc: "+ job.getOrderCode() +"] Hệ thống đã ghi nhận bạn đã đến địa điểm làm việc cho công việc " + serviceName.toLowerCase();
 		NotificationDTO cleanerNotification = new NotificationDTO();
@@ -940,7 +940,7 @@ public class CleanerJobService {
 		customerNotification.setTimestamp(LocalDateTime.now());
 		customerNotification.setRead(false);
 		notificationService.processNotification(customerNotification, "CUSTOMER", Math.toIntExact(job.getCustomer().getId()));
-		// smsService.sendSms(job.getCustomer().getPhone(), message);
+		smsService.sendSms(job.getCustomer().getPhone(), message);
 
 		String message1 = "[Mã công việc: "+ job.getOrderCode() +"] Yêu cầu xác nhận hoàn thành công việc " + serviceName.toLowerCase() + " Đã được gửi tới chủ nhà.";
 		NotificationDTO cleanerNotification = new NotificationDTO();
@@ -2136,7 +2136,7 @@ public class CleanerJobService {
 		cleanerNotification.setTimestamp(LocalDateTime.now());
 		cleanerNotification.setRead(false); // ✅ set read = false
 		notificationService.processNotification(cleanerNotification, "CLEANER", Math.toIntExact(cleanerId));
-		// smsService.sendSms(cleaner.getPhone(), message_cleaner);
+		smsService.sendSms(cleaner.getPhone(), message_cleaner);
 
 		return ResponseEntity.ok(Map.of(
 				"message", "Tạo công việc thành công",
@@ -2478,7 +2478,7 @@ public class CleanerJobService {
 			customerNotification.setTimestamp(LocalDateTime.now());
 			customerNotification.setRead(false);
 			notificationService.processNotification(customerNotification, "CUSTOMER", Math.toIntExact(job.getCustomer().getId()));
-			// smsService.sendSms(job.getCustomer().getPhone(), customerMessage);
+			smsService.sendSms(job.getCustomer().getPhone(), customerMessage);
 
 			NotificationDTO cleanerNotification = new NotificationDTO();
 			cleanerNotification.setUserId(cleaner.getId());
